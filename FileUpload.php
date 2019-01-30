246f0d1e0833047c4e2aeab506155866942a314c
Deepthi H Lakshminarayana <Deepthi.hl10@gmail.com>
2019-01-30 14:48:29 -0500

    Create FileUpload.php

---
+++ FileUpload.php
@@ -0,0 +1,5 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select bulk template to upload (CSV Only):
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Bulk CSV" name="submit">
+</form>
